1b60f40ba449daf7d6af96dd790c45f3aae0ce52
hiramekun <thescript1210@gmail.com>
2023-08-24 22:35:33 +0900

    p.56: map.go

---
+++ ch03/map.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nilMap map[string]int
+	fmt.Println(len(nilMap))   // 0
+	fmt.Println(nilMap["abc"]) // 0
+	// nilMap["abc"] = 123
+	totalWins := map[string]int{}
+	fmt.Println(totalWins == nil) // false
+	fmt.Println(totalWins["abc"]) // 0
+	totalWins["abc"] = 3
+	fmt.Println(totalWins["abc"]) // 3
+
+	teams := map[string][]string{
+		"ライターズ":       []string{"夏目", "森", "国木田"},
+		"ナイツ":           []string{"武田", "徳川", "明智"},
+		"ミュージシャンズ": []string{"ラベル", "ベートーベン", "リスト"},
+	}
+
+	ages := make(map[string]int, 10)
+}
