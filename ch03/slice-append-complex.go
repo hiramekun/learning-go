c93c36e76e8bf91a82eb6fbc8941cd2a1d193a35
hiramekun <thescript1210@gmail.com>
2023-08-24 20:34:58 +0900

    p.49: change to full slice

--- ch03/slice-append-complex.go
+++ ch03/slice-append-complex.go
@@ -6,8 +6,8 @@ func main() {
 	x := make([]int, 0, 5)
 	x = append(x, 1, 2, 3, 4)
 
-	y := x[:2]
-	z := x[2:]
+	y := x[:2:2] // フルスライス
+	z := x[2:4:4]
 	fmt.Println(cap(x), cap(y), cap(z))
 
 	y = append(y, 30, 40, 50)
