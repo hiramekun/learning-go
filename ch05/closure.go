0dcaa913e7744276f28f6a9c5673bc107077d506
hiramekun <thescript1210@gmail.com>
2023-08-25 18:09:02 +0900

    p.105: closure.go

---
+++ ch05/closure.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Person struct {
+	FirstName string
+	LastName  string
+	Age       int
+}
+
+func main() {
+	people := []Person{
+		{"Pat", "Patterson", 37},
+		{"Tracy", "Bobbert", 23},
+		{"Fred", "Fredson", 18},
+	}
+
+	fmt.Println("初期データ")
+	fmt.Println(people)
+
+	sort.Slice(people, func(i, j int) bool {
+		return people[i].LastName < people[j].LastName
+	})
+	fmt.Println("姓でソート")
+	fmt.Println(people)
+}
