e015c4676e3bba80d9173781f43c122163109ef6
hiramekun <thescript1210@gmail.com>
2023-08-27 16:13:23 +0900

    p.109: DoSomeInserts

--- ch05/defer.go
+++ ch05/defer.go
@@ -1,10 +1,35 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"io"
 	"log"
 	"os"
 )
+
+func DoSomeInserts(ctx context.Context, db *sql.DB, value1, value2 string) (err error) {
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err == nil {
+			err = tx.Commit()
+		}
+		if err != nil {
+			tx.Rollback()
+		}
+	}()
+	_, err = tx.ExecContext(ctx, "INSERT INTO FOO (val) values $1", value1)
+	if err != nil {
+		return err
+	}
+
+	// tx を使ってさらにデータベースに書き込むコードをここに追加する
+	return nil
+}
 
 func main() {
 	if len(os.Args) < 2 {
