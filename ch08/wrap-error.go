86998af2d855ba58fc4670d759f64699b6502a82
hiramekun <thescript1210@gmail.com>
2023-08-29 00:44:55 +0900

    p.181: wrap-error.go

---
+++ ch08/wrap-error.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+func fileChecker(name string) error {
+	f, err := os.Open(name)
+	if err != nil {
+		return fmt.Errorf("in fileChecker: %w", err)
+	}
+	f.Close()
+	return nil
+}
+
+func main() {
+	err := fileChecker("not_here.txt")
+	if err != nil {
+		fmt.Println(err)
+		if wrappedErr := errors.Unwrap(err); wrappedErr != nil {
+			fmt.Println("in main:", wrappedErr)
+		}
+	}
+	/**
+	in fileChecker: open not_here.txt: no such file or directory
+	in main: open not_here.txt: no such file or directory
+	*/
+}
