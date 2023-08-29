0d693c0e95d9a7adf9622f91b29444a630ddc3f7
hiramekun <thescript1210@gmail.com>
2023-08-29 09:25:20 +0900

    p.184: implement custom is

--- ch08/is-as.go
+++ ch08/is-as.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"reflect"
 )
 
 func fileChecker(name string) error {
@@ -13,6 +14,43 @@ func fileChecker(name string) error {
 	}
 	f.Close()
 	return nil
+}
+
+type MyErr struct {
+	Codes []int
+}
+
+func (me MyErr) Error() string {
+	return fmt.Sprintf("codes: %v", me.Codes)
+}
+
+func (me MyErr) Is(taget error) bool {
+	if me2, ok := taget.(MyErr); ok {
+		return reflect.DeepEqual(me, me2)
+	}
+	return false
+}
+
+type ResourceErr struct {
+	Resource string
+	Code     int
+}
+
+func (re ResourceErr) Error() string {
+	return fmt.Sprintf("%s: %d", re.Resource, re.Code)
+}
+
+func (re ResourceErr) Is(target error) bool {
+	if other, ok := target.(ResourceErr); ok {
+		ignoreResource := other.Resource == ""
+		ignoreCode := other.Code == 0
+		matchResource := other.Resource == re.Resource
+		matchCode := other.Code == re.Code
+		return matchResource && matchCode ||
+			matchResource && ignoreCode ||
+			ignoreResource && matchCode
+	}
+	return false
 }
 
 func main() {
