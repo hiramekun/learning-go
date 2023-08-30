171f3c872e75d60c5efca99c6026448c4deba076
hiramekun <thescript1210@gmail.com>
2023-08-30 23:03:20 +0900

    p.265: json2.go

---
+++ ch11/json2.go
@@ -0,0 +1,15 @@
+package main
+
+import "time"
+
+type Order struct {
+	ID          string    `json:"id"`
+	DataOrdered time.Time `json:"date_ordered"`
+	CustomerID  string    `json:"customer_id"`
+	Items       []Item    `json:"items"`
+}
+
+type Item struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
