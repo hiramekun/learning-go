f22f16b886cfa04e2c22c4db9b3ae5b72cfcdf3e
hiramekun <thescript1210@gmail.com>
2023-08-31 02:16:24 +0900

    p.287: cancel.go

---
+++ ch12/cancel.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"sync"
+	"time"
+)
+
+func slowServer() *httptest.Server {
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(2 * time.Millisecond)
+		w.Write([]byte("Slow response"))
+	}))
+	return s
+}
+
+func fastServer() *httptest.Server {
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Query().Get("error") == "true" {
+			w.Write([]byte("error"))
+			return
+		}
+		w.Write([]byte("ok"))
+	}))
+	return s
+}
+
+var client = http.Client{}
+
+func callBoth(ctx context.Context, errVal string, slowURL string, fastURL string) {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		err := callServer(ctx, "slow", slowURL)
+		if err != nil {
+			cancel()
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		err := callServer(ctx, "fast", fastURL+"?error="+errVal)
+		if err != nil {
+			cancel()
+		}
+	}()
+
+	wg.Wait()
+	fmt.Println("both end")
+}
+
+func callServer(ctx context.Context, label string, url string) error {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		fmt.Println(label, "error from request:", err)
+		return err
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(label, "error from response:", err)
+		return err
+	}
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(label, "error reading body:", err)
+		return err
+	}
+	result := string(data)
+	if result != "" {
+		fmt.Println(label, "result:", result)
+	}
+	if result == "error" {
+		fmt.Println("cancelling context from", label)
+		return errors.New("error!")
+	}
+	return nil
+}
+
+/*
+➜ go run ./cancel.go true
+fast result: error
+cancelling context from fast
+slow error from response: Get "http://127.0.0.1:58541": context canceled
+both end
+
+➜ go run ./cancel.go false
+fast result: ok
+slow result: Slow response
+both end
+*/
+func main() {
+	ss := slowServer()
+	defer ss.Close()
+	fs := fastServer()
+	defer fs.Close()
+
+	ctx := context.Background()
+	callBoth(ctx, os.Args[1], ss.URL, fs.URL)
+}
