0148e0de13f1333d9e9dd0eb0d9da4d52e5e89cc
hiramekun <thescript1210@gmail.com>
2023-09-05 00:16:34 +0900

    p.363: differ.go

---
+++ ch15/differ.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type Pair[T fmt.Stringer] struct {
+	Val1 T
+	Val2 T
+}
+
+type Differ[T any] interface {
+	fmt.Stringer
+	Diff(T) float64
+}
+
+func FindCloser[T Differ[T]](pair1, pair2 Pair[T]) Pair[T] {
+	d1 := pair1.Val1.Diff(pair1.Val2)
+	d2 := pair2.Val1.Diff(pair2.Val2)
+
+	if d1 < d2 {
+		return pair1
+	}
+	return pair2
+}
